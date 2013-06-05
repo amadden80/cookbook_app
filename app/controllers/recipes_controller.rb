9fb5321e2a176fa38b0553c7686b15ca98799879
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-05 16:31:09 -0400

    Adding views files for Recipes, including selector on new recipe page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,21 +5,38 @@ class RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.new(params[:recipe])
+    if recipe.save
+      redirect_to recipes_path
+    else
+      redirect_to new_recipe_path
+    end
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    recipe = Recipe.find(params[:id])
+    if recipe.update_attributes(params[:recipe])
+      redirect_to recipes_path
+    else
+      redirect_to edit_recipe_path
+    end
   end
 
   def destroy
+    Recipe.find(params[:id]).delete
+    redirect_to recipes_path
   end
 
 end
